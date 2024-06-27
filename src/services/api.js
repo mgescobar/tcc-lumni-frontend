df6c71f6a2a5201335eae0733aed7d499f29b4b6
Matheus Garcia Escobar <mgescobar@inf.ufsm.br>
2024-06-27 01:32:50 -0300

    fix to work with deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const apiUrl = "http://localhost:3333/";
+// const apiUrl = "http://localhost:3333/";
+const apiUrl = "https://tcc-lumni-backend-c51874ccfe49.herokuapp.com/";
 
 const api = axios.create({
   baseURL: apiUrl,
